39c7177a8d2866dc88d785e2fdcfd1605596797f
JO <wgtjdwls2002@bu.ac.kr>
2026-04-20 12:41:56 +0900

    시제품 공유

---
+++ src/components/MatchCard.tsx
@@ -0,0 +1,26 @@
+
+
+interface props{
+    name: string;
+    distance: number;
+    matchRate: number;
+}
+
+interface Props {
+  name: string;
+  distance: number;
+  matchRate: number;
+}
+
+const MatchCard = ({ name, distance, matchRate }: Props) => {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <p>거리: {distance} km</p>
+      <p>매칭률: {matchRate}%</p>
+      <button>선택</button>
+    </div>
+  );
+};
+
+export default MatchCard;
